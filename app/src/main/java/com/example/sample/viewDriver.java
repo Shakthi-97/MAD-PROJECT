939633acba50d43464568ccc73300c004358456f
Thakshayini <kamalanathan58@gmail.com>
2020-08-16 01:00:59 +0530

    check my idea...

--- app/src/main/java/com/example/sample/viewDriver.java
+++ app/src/main/java/com/example/sample/viewDriver.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class viewDriver extends AppCompatActivity {
-
+/*sfjhdljhbldjvgh*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
